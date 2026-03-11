29f51fd5fa774db7a63e804aaf81847d811f2aee
Wathisa <wathisa@gmail.com>
2026-03-11 18:35:02 +0700

    complete SQL exercises

--- posgresql/query.sql
+++ posgresql/query.sql
@@ -1,1 +1,7 @@
-SELECT * FROM Staff
+SELECT ingredients.name
+FROM ingredients
+JOIN recipeitems
+ON ingredients.ingredient_id = recipeitems.ingredient_id
+JOIN menuitems
+ON recipeitems.item_id = menuitems.item_id
+WHERE menuitems.name = 'Bacon Cheeseburger';
